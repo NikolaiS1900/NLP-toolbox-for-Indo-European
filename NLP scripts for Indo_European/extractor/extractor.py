a2e41cb69a2149dfbe7534c0ee4d37cfb23edc2c
Nikolai Brink Sandbeck <sandbeck1990@protonmail.com>
2021-11-08 14:59:53 +0100

    Update extractor.py

--- NLP scripts for Indo_European/extractor/extractor.py
+++ NLP scripts for Indo_European/extractor/extractor.py
@@ -1,10 +1,16 @@
 import glob, os, re
 from path_finder import path_finder
 
-## Uncomment to activate. You can only activate one at the time.
-# from Albanian import Sounds
-# from Proto_Germanic import Sounds
-from Danish_Norwegian_Swedish import Sounds
+## Uncomment to activate. You can only activate one at the time. (Uncomment = remove hash tag)
+#from Albanian import Sounds
+#from Danish_Norwegian_Swedish import Sounds
+#from Faroese import Sounds
+#from Icelandic import Sounds
+#from Latin import Sounds
+#from Proto_Germanic import Sounds
+#from Old_Church_Slavonic import Sounds
+#from Old_Danish import Sounds
+#from Old_Icelandic import Sounds
 
 CWD = os.getcwd()
 PATH = path_finder()
@@ -19,6 +25,7 @@ print('''
 Tast 1 for søgning i forlyd (absolut forlyd).
 Tast 2 for søgning i indlyd.
 Tast 3 for søgning i udlyd (absolut udlyd).
+Tast 4 for søgning i det hele af ordene.
 
 Søgningen vil finde alle ord der opfylder dine søgekriterier, og gemme disse ord i en seperat tekstfil.
 ''')
@@ -34,6 +41,8 @@ def start():
         indlyd()
     elif valg == "3":
         udlyd()
+    elif valg == "4":
+        hel_ord()
     else:
         exit(0)
 
@@ -122,6 +131,35 @@ Ved både 2 og 3 kan du vælge at ikke indtaste noget sælg, bare vælge en lydg
     result = re.findall(pattern, read_tekst, re.IGNORECASE)
     navn = "udlyd"
     make_file(result, navn)
+
+
+def hel_ord():
+    print("""
+Her kan du søge på i indlyd. Du har nu følgende valg; indtaster du:
+
+1     Frisøgning
+2     Frisøgning + Lydgruppe
+3     Lydgruppe + Frisøgning
+
+Ved både 2 og 3 kan du vælge at ikke indtaste noget sælg, bare vælge en lydgruppe.
+""")
+
+    indput = input("\nIndtast dit valg: ")
+    if indput == "1":
+        indput = input("\nSkriv dit ord med eller uden regular expression: ")
+        pattern = r"\b\w*"+indput+r"\w*"
+    elif indput == "2":
+        indput = input("Skriv dit ord med eller uden regular expression: ")
+        pattern = r"\b\w*"+indput+Sounds()+r"\w*"
+    elif indput == "3":
+        indput = input("Skriv dit ord med eller uden regular expression: ")
+        pattern = r"\b\w*"+Sounds()+indput+r"\w*"
+    else:
+        exit(0)
+    result = re.findall(pattern, read_tekst, re.IGNORECASE)
+    navn = "hel_ord"
+    make_file(result, navn)
+
 
 def make_file(result, navn):
     set_tekst = set(result)
