9c683b99d48a09990e107a1decb571182af44e45
Nikolai Brink Sandbeck <sandbeck1990@protonmail.com>
2022-02-05 09:22:45 +0100

    Update word2vec.py

--- NLP scripts for Indo_European/word2vec/word2vec.py
+++ NLP scripts for Indo_European/word2vec/word2vec.py
@@ -1,33 +1,43 @@
 import gensim, os, glob
 from gensim import models
-from path_finder import path_finder
 import matplotlib.pyplot as plt
 
 CWD = os.getcwd()
-PATH = path_finder()
+# Går i overmappen.
+PATH = os.path.dirname(CWD) 
 
+# Går til mappen preprocess og så videre ned i preprocessed_text, og tager teksfilen
+# der ligger der, og læser den ind.
 for filename in glob.glob(PATH+'/preprocess/preprocessed_text/*.txt'):
 	with open(filename, 'r', encoding="utf8") as f:
 		READ_TEXT = f.read()
 
 
-# preprocess text
-PREPROCESSED_TEXT = READ_TEXT.lower()
+# Gør alle bogstaver små
+#LOWER_CASE = READ_TEXT.lower()
 
-# list of list
-PREPROCESSED_TEXT = [READ_TEXT.split()]
+# Moddellen Word2Vec skal bruge en liste af lister.
+LIST_OF_LISTS = [READ_TEXT.split()]
 
-# train word embeddings model
-MODEL = gensim.models.Word2Vec(PREPROCESSED_TEXT, vector_size=2, window=5, min_count=1, workers=2, sg=1)
+# Kører moddellen. Husk at window størrelsen kan justeres.
+# Hvis den er sat til 3, vil hvert ords vektorværdi blive påvirket af de næste tre ord frem og de forrige tre ord tilbage.
+MODEL = gensim.models.Word2Vec(LIST_OF_LISTS, vector_size=2, window=3, min_count=0, workers=2, sg=1)
 
-WORD1 = MODEL.wv['kat'] # blue
-WORD2 = MODEL.wv['kat'] # orange
-WORD3 = MODEL.wv['kat'] # purple
+# Her kan man skrive hvilken værdi ens variabler skal have.
+WORD_1 = 'bonde' # blue
+WORD_2 = 'konung' # orange
+WORD_3 = 'man' # purple
 
-print(f"vector of WORD1: {WORD1}")
-print(f"vector of WORD2: {WORD2}")
-print(f"vector of WORD3: {WORD3}")
+WORD1 = MODEL.wv[WORD_1] # blå
+WORD2 = MODEL.wv[WORD_2] # orange
+WORD3 = MODEL.wv[WORD_3] # lilla
 
+# Gør et label til x-aksen klar.
+XLABEL = f"""
+vector of {WORD_1} (blue arrow): {WORD1}\n
+vector of {WORD_2} (orange arrow): {WORD2}\n
+vector of {WORD_3} (purple arrow): {WORD3}
+"""
 
 ## plot vectors
 ## comment to deactive and uncomment to deactive
@@ -50,6 +60,6 @@ try:
 except:
   pass
 
-
+plt.xlabel(XLABEL)
 plt.axis([-5, 5, -5, 5])
 plt.show()
