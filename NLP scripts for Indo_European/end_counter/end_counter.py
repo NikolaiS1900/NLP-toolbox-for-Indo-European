66841380d1fc33a4c96512c5cda66ad05d127ad5
Nikolai Brink Sandbeck <sandbeck1990@protonmail.com>
2022-02-05 09:16:35 +0100

    Update end_counter.py

--- NLP scripts for Indo_European/end_counter/end_counter.py
+++ NLP scripts for Indo_European/end_counter/end_counter.py
@@ -16,5 +16,26 @@ END_LIST = []
 for item in SPLIT_TEXT:
     END_LIST.append(item[-1])
 
+RESULT = []
+
 for val, freq in sorted(Counter(END_LIST).items()):
-    print(val, ' : ', freq)
+    RESULT.append(f"-{val} : {freq}")
+
+NICE_LIST = '\n\n'.join(RESULT)
+
+PRINT = open(CWD+'/Result/end_count_list.txt', 'w', encoding='utf8')
+PRINT.write('' + NICE_LIST)
+PRINT.close()
+
+
+print("""
+end_counter.py     done running:
+
+end_count_list.txt is ready in the Result directory.
+
+Visit my GitHub at: https://github.com/NikolaiS1900
+I can be contacted via: sandbecks_github@protonmail.com
+
+Kind regards
+- Nikolai Sandbeck
+""")
